e09dc65aa688726c43299e421545660e901e2413
fakefish <fakefish@fakefishtekiMacBook-Pro.local>
2014-09-29 00:16:26 +0800

    调整文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,34 @@
 module.exports = function(grunt) {
 
-	// config
-	// grunt.initConfig({
-	// 	pkg: grunt.file.readJSON('package.json'),
-	// 	uglify: {
-	// 		options: {
-	// 			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-	// 		},
-	// 		build: {
-	// 			src: 'src/<%= pkg.name %>.js',
-	// 			dest: 'build/<%= pkg.name %>.min.js'
-	// 		}
-	// 	}
-	// });
+  // config
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    // concat: {
+    //   dist: {
+    //     src: ['<banner>', 'src/audio.js', 'src/scene.js', 'src/touch.js', 'src/main.js'],
+    //     dest: 'mobilepage.js',
+    //   }
+    // }
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
 
-	// grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	// // default task
-	// grunt.registerTask('default', ['uglify']);
+  // default task
+  grunt.registerTask('default', ['uglify']);
 }
